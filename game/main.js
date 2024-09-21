553b484946700899ebfc3598ec72deffc24b1230
Blaxolot <marek.dzikowski11@gmail.com>
2024-09-21 12:25:59 +0200

    jump on left click get smaller on right click

--- game/main.js
+++ game/main.js
@@ -64,27 +64,29 @@ scene("game", () => {
   function addHamster() {
     updateVariables();
     player = add([
-      sprite(isWhite + updateHamster(), { width: hamster_width }),
+      sprite(isWhite + updateHamster(), {width: hamster_width}),
       pos(hamster_pos, -65),
       anchor("center"),
-      area({ scale: vec2(0.7, 1) }),
+      area({scale: vec2(0.7, 1)}),
       body(),
       doubleJump(),
       "player",
     ]);
     hamster_accessories.forEach(item => {
-      const { Scale, Scale2, scale1, scale2 } = coolList[item];
+      const {Scale, Scale2, scale1, scale2} = coolList[item];
       if (eval(item) == true) {
         player.add([
-          sprite(item, { width: hamster_width / 2 }),
-          scale(vec2(currentHamster == "hamster" ? Scale : (Scale2 || Scale))),
+          sprite(item, {width: hamster_width / 2}),
+          scale(vec2(currentHamster == "hamster" ? Scale : Scale2 || Scale)),
           anchor("center"),
           pos(0, -hamster_width / (currentHamster == "hamster" ? scale1 : scale2)),
         ]);
       }
     });
     // get smaller when user presses down or s
-    onKeyDown(["down", "s"], () => player.scale = vec2(0.5));
+    onMousePress("right", () => (player.scale = vec2(0.5)));
+    onMouseRelease("right", () => (player.scale = vec2(1)));
+    onKeyDown(["down", "s"], () => (player.scale = vec2(0.5)));
     onKeyRelease(["down", "s"], () => {
       localStorage.setItem("number_of_reductions", ++number_of_reductions);
       player.scale = vec2(1);
@@ -95,7 +97,7 @@ scene("game", () => {
   function shortJump() {
     if (player.isGrounded()) {
       player.jump(600);
-      play("jump", { volume: 2 });
+      play("jump", {volume: 2});
       lastJumpFunction = shortJump;
       localStorage.setItem("number_of_jumps", ++number_of_jumps);
     }
@@ -103,7 +105,7 @@ scene("game", () => {
   function longJump() {
     if (player.isGrounded()) {
       player.jump(750);
-      play("jump", { volume: 2 });
+      play("jump", {volume: 2});
       lastJumpFunction = longJump;
       localStorage.setItem("number_of_jumps", ++number_of_jumps);
     }
@@ -131,8 +133,12 @@ scene("game", () => {
       return false;
     }
     // desktop
-    document.addEventListener("mouseup", clearPressTimer);
-    document.addEventListener("mousedown", setPressTimer);
+    document.addEventListener("mouseup", event => {
+      if (event.button === 0) clearPressTimer();
+    });
+    document.addEventListener("mousedown", event => {
+      if (event.button === 0) setPressTimer();
+    });
     // mobile phone
     document.addEventListener("touchstart", setPressTimer);
     document.addEventListener("touchend", clearPressTimer);
@@ -140,11 +146,11 @@ scene("game", () => {
     function Jumping() {
       player.doubleJump(650);
     }
-    onClick(() => Jumping());
+    onMousePress("left", () => Jumping());
     onKeyPress(["space", "up", "w"], () => Jumping());
 
     player.onDoubleJump(() => {
-      play("jump", { volume: 2 });
+      play("jump", {volume: 2});
       localStorage.setItem("number_of_jumps", ++number_of_jumps);
     });
   }
--- shop/UI.js
+++ shop/UI.js
@@ -1,121 +1,120 @@
+function createBoxes() {
+  items.forEach(item => {
+    const shop_phone = width() < 450;
+    const {food1, food2, price1, price2, Scale} = itemPricing[item];
+    const numberOfColumns = width() < 660 ? 2 : 4;
+
+    let position = [0, 0];
+    const itemIndex = items.indexOf(item);
+    if (itemIndex !== -1) {
+      const row = Math.floor(itemIndex / numberOfColumns);
+      const col = itemIndex % numberOfColumns;
+      position = [
+        (shop_phone ? 5 : 20) + col * (shop_phone ? 175 : 220),
+        (shop_phone ? 5 : 20) + row * (shop_phone ? 175 : 220),
+      ];
+    }
+
+    eval(`${item}_box = add([
+    rect(200, 200, { radius: 15 }),
+    pos(position),
+    color(100, 100, 100),
+    scale(shop_phone ? 0.8:1)
+    ])`);
+
+    const isOneFood = !food2;
+    const first_food = getSingularFood(food1);
+    const second_food = getSingularFood(food2);
+
+    let mainItem = ["shoes", "gloves"].includes(item)
+      ? currentHamster + "_" + item
+      : item;
+
+    (apple = 0.085), (banana = [-0.085, 0.085]);
+    (seed = 0.08), (tomato = 0.08);
+    let coolPos = first_food == "banana" ? 54 : isOneFood ? 46 : 10;
+    eval(
+      `${item}_box.add([sprite("${first_food}"), scale(${first_food}), pos(${coolPos},5)])`
+    );
+    eval(`${item}_box.add([text(price1), scale(0.9), pos(isOneFood ? 90: 55, 13)])`);
+    !isOneFood &&
+      eval(`${item}_box.add([
+      sprite("${second_food}"),
+      scale(${second_food}),
+      pos(second_food == "banana" ? 149:105,5),
+    ])`);
+    eval(`${item}_box.add([text(price2 || ""), scale(0.9), pos(150, 13)])`);
+
+    if (!(item == "mystery" && localStorage.getItem("?*$?r2?") == "$q?q&?a??")) {
+      window[`${item}_box`].add([
+        sprite(mainItem),
+        pos(100, 100),
+        scale(Scale),
+        anchor("center"),
+      ]);
+    }
+
+    item == "x2_hearts" &&
+      x2_hearts_box.add([
+        text("2x"),
+        scale(0.9),
+        color(BLACK),
+        pos(100, 100),
+        anchor("center"),
+      ]);
+    item == "mystery" &&
+      localStorage.getItem("?*$?r2?") == "$q?q&?a??" &&
+      mystery_box.add([
+        text(polish ? "Podwójny Skok" : "Double Jump", {
+          width: 200,
+          size: 30,
+          align: "center",
+        }),
+        color(BLACK),
+        pos(100, 100),
+        anchor("center"),
+      ]);
+  });
+}
+// create buttons with corresponding text, color and size
+function createButtons() {
+  items.forEach(item => {
+    const condition = itemConditions[item];
+    if (localStorage.getItem(condition.key) == condition.value) {
+      const status = eval(condition.var) == "True";
+      eval(
+        `${item}_text = status ? (polish ? "Ubrane" : "Wearing") : (polish ? "Ubierz" : "Wear")`
+      );
+      eval(`buy_${item}_button_color = status ? rgb(0, 160, 0) : rgb(200, 0, 0)`);
+      eval(`buy_${item}_text_scale = status ? 0.6 : 0.7`);
+    } else {
+      const {food1, food2, price1, price2} = itemPricing[item];
+      const con = eval(food1) < price1 || eval(food2) < price2;
+      eval(`${item}_text = polish ? "Kup" : "Buy"`);
+      eval(`buy_${item}_button_color = con ? rgb(250, 25, 25) : rgb(0, 200, 0)`);
+      eval(`buy_${item}_text_scale = 0.7`);
+    }
+    // add buttons
+    window[`buy_${item}`] = eval(`${item}_box`).add([
+      rect(100, 35, {radius: 8}),
+      color(window[`buy_${item}_button_color`]),
+      pos(100, 175),
+      area(),
+      anchor("center"),
+      outline(4.5),
+    ]);
+    window[`buy_${item}_text`] = window[`buy_${item}`].add([
+      text(window[`${item}_text`]),
+      anchor("center"),
+      scale(window[`buy_${item}_text_scale`]),
+      color(BLACK),
+    ]);
+    // hovers
+    MyHover(window[`buy_${item}`], 1.025, 1);
+  });
+}
 function shop_UI() {
   createBoxes();
-  function createBoxes() {
-    items.forEach(item => {
-      const shop_phone = width() < 450;
-      const {food1, food2, price1, price2, Scale} = itemPricing[item];
-      const numberOfColumns = width() < 660 ? 2 : 4;
-
-      let position = [0, 0];
-      const itemIndex = items.indexOf(item);
-      if (itemIndex !== -1) {
-        const row = Math.floor(itemIndex / numberOfColumns);
-        const col = itemIndex % numberOfColumns;
-        position = [
-          (shop_phone ? 5 : 20) + col * (shop_phone ? 175 : 220),
-          (shop_phone ? 5 : 20) + row * (shop_phone ? 175 : 220),
-        ];
-      }
-
-      eval(`${item}_box = add([
-      rect(200, 200, { radius: 15 }),
-      pos(position),
-      color(100, 100, 100),
-      scale(shop_phone ? 0.8:1)
-      ])`);
-
-      const isOneFood = !food2;
-      const first_food = getSingularFood(food1);
-      const second_food = getSingularFood(food2);
-
-      let mainItem = ["shoes", "gloves"].includes(item)
-        ? currentHamster + "_" + item
-        : item;
-
-      (apple = 0.085), (banana = [-0.085, 0.085]);
-      (seed = 0.08), (tomato = 0.08);
-      let coolPos = first_food == "banana" ? 54 : isOneFood ? 46 : 10;
-      eval(
-        `${item}_box.add([sprite("${first_food}"), scale(${first_food}), pos(${coolPos},5)])`
-      );
-      eval(`${item}_box.add([text(price1), scale(0.9), pos(isOneFood ? 90: 55, 13)])`);
-      !isOneFood &&
-        eval(`${item}_box.add([
-        sprite("${second_food}"),
-        scale(${second_food}),
-        pos(second_food == "banana" ? 149:105,5),
-      ])`);
-      eval(`${item}_box.add([text(price2 || ""), scale(0.9), pos(150, 13)])`);
-
-      if (!(item == "mystery" && localStorage.getItem("?*$?r2?") == "$q?q&?a??")) {
-        window[`${item}_box`].add([
-          sprite(mainItem),
-          pos(100, 100),
-          scale(Scale),
-          anchor("center"),
-        ]);
-      }
-
-      item == "x2_hearts" &&
-        x2_hearts_box.add([
-          text("2x"),
-          scale(0.9),
-          color(BLACK),
-          pos(100, 100),
-          anchor("center"),
-        ]);
-      item == "mystery" &&
-        localStorage.getItem("?*$?r2?") == "$q?q&?a??" &&
-        mystery_box.add([
-          text(polish ? "Podwójny Skok" : "Double Jump", {
-            width: 200,
-            size: 30,
-            align: "center",
-          }),
-          color(BLACK),
-          pos(100, 100),
-          anchor("center"),
-        ]);
-    });
-  }
-
-  // create buttons with corresponding text, color and size
-  function createButtons() {
-    items.forEach(item => {
-      const condition = itemConditions[item];
-      if (localStorage.getItem(condition.key) == condition.value) {
-        const status = eval(condition.var) == "True";
-        eval(
-          `${item}_text = status ? (polish ? "Ubrane" : "Wearing") : (polish ? "Ubierz" : "Wear")`
-        );
-        eval(`buy_${item}_button_color = status ? rgb(0, 160, 0) : rgb(200, 0, 0)`);
-        eval(`buy_${item}_text_scale = status ? 0.6 : 0.7`);
-      } else {
-        const {food1, food2, price1, price2} = itemPricing[item];
-        const con = eval(food1) < price1 || eval(food2) < price2;
-        eval(`${item}_text = polish ? "Kup" : "Buy"`);
-        eval(`buy_${item}_button_color = con ? rgb(250, 25, 25) : rgb(0, 200, 0)`);
-        eval(`buy_${item}_text_scale = 0.7`);
-      }
-      // add buttons
-      window[`buy_${item}`] = eval(`${item}_box`).add([
-        rect(100, 35, {radius: 8}),
-        color(window[`buy_${item}_button_color`]),
-        pos(100, 175),
-        area(),
-        anchor("center"),
-        outline(4.5),
-      ]);
-      window[`buy_${item}_text`] = window[`buy_${item}`].add([
-        text(window[`${item}_text`]),
-        anchor("center"),
-        scale(window[`buy_${item}_text_scale`]),
-        color(BLACK),
-      ]);
-      // hovers
-      MyHover(window[`buy_${item}`], 1.025, 1);
-    });
-  }
   createButtons();
   x = add([text("x"), area(), pos(width() - 50, 15), color(LIGHT_GRAY)]);
   x.onClick(() => go("menu"));
