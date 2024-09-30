1e896bcaa374021f6adc6852d671def67332f460
Blaxolot <marek.dzikowski11@gmail.com>
2024-09-30 11:38:22 +0200

    Menu text is bigger and hides when overlapping

--- menu/hamster.js
+++ menu/hamster.js
@@ -1,44 +1,56 @@
 const hamster_accessories = ["winter_hat", "cap", "glasses"];
 const coolList = {
-    winter_hat: { Scale: [0.9, 0.65], scale1: 1.925, scale2: 2 },
-    cap: { Scale: [0.9, 0.7], scale1: 2, scale2: 2.05 },
-    glasses: { Scale: [0.75, 0.7], Scale2: [0.8, 0.7], scale1: 2.85, scale2: 3.5 },
+  winter_hat: { Scale: [0.9, 0.65], scale1: 1.925, scale2: 2 },
+  cap: { Scale: [0.9, 0.7], scale1: 2, scale2: 2.05 },
+  glasses: {
+    Scale: [0.75, 0.7],
+    Scale2: [0.8, 0.7],
+    scale1: 2.85,
+    scale2: 3.5,
+  },
 };
 function initialize_hamster() {
-    hamster_width = phone ? 250 : 285;
-    Hamster_text_size = phone ? 0.01 : 80;
+  hamster_width = phone ? 250 : 285;
+  Hamster_text_size = phone ? 0.01 : 100;
+  // collider
+  add([rect(150, height()), area(), opacity(0)]);
 
-    add([
-        text(MenuText, {
-            size:
-                Hamster_text_size -
-                ((MenuText == "Game Over" || MenuText == "Koniec Gry") && !phone && 15),
-        }),
-        pos(width() / 2, height() / 2 - 230),
-        anchor("center"),
-    ]);
+  const Hamster_text = add([
+    text(MenuText, { size: Hamster_text_size }),
+    pos(width() / 2, height() / 2 - 250),
+    anchor("center"),
+    area(),
+  ]);
+  // when text collides with collider destroy it
+  Hamster_text.onCollide(() => destroy(Hamster_text));
 }
 
 function AddHamster(white = "") {
-    white !== "" ? (isWhite = "white_") : (isWhite = "");
-    if (white !== "" && !getSprite(white + updateHamster())) {
-        loadSprite(white + updateHamster(), `images/menu/${white + updateHamster()}.png`);
-    }
-    menuHamster = add([
-        sprite(white + updateHamster(), { width: hamster_width }),
-        pos(width() / 2, height() / 2 + ((winter_hat || cap) == true && 25)),
-        anchor("center"),
-    ]);
+  white !== "" ? (isWhite = "white_") : (isWhite = "");
+  if (white !== "" && !getSprite(white + updateHamster())) {
+    loadSprite(
+      white + updateHamster(),
+      `images/menu/${white + updateHamster()}.png`,
+    );
+  }
+  menuHamster = add([
+    sprite(white + updateHamster(), { width: hamster_width }),
+    pos(width() / 2, height() / 2 + ((winter_hat || cap) == true && 25)),
+    anchor("center"),
+  ]);
 
-    hamster_accessories.forEach(item => {
-        const { Scale, Scale2, scale1, scale2 } = coolList[item];
-        if (eval(item) == true) {
-            menuHamster.add([
-                sprite(item, { width: hamster_width / 2 }),
-                scale(vec2(currentHamster == "hamster" ? Scale : (Scale2 || Scale))),
-                anchor("center"),
-                pos(0, -hamster_width / (currentHamster == "hamster" ? scale1 : scale2)),
-            ]);
-        }
-    });
+  hamster_accessories.forEach(item => {
+    const { Scale, Scale2, scale1, scale2 } = coolList[item];
+    if (eval(item) == true) {
+      menuHamster.add([
+        sprite(item, { width: hamster_width / 2 }),
+        scale(vec2(currentHamster == "hamster" ? Scale : Scale2 || Scale)),
+        anchor("center"),
+        pos(
+          0,
+          -hamster_width / (currentHamster == "hamster" ? scale1 : scale2),
+        ),
+      ]);
+    }
+  });
 }
